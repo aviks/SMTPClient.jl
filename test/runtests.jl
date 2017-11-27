4fb7d1616ce53e7a5fee9124c3b0c3b8694932d2
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-11-27 15:11:04 +0800

    Error message for Humans(TM)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,19 @@
+using Base.Test
+
 using SMTPClient
+
+
+@testset "Error message for Humans(TM)" begin
+    let errmsg = "Couldn't resolve host name"
+        o = SendOptions()
+        server = "smtp://noneists"
+        body = IOBuffer("test")
+
+        try
+            SMTPClient.send(server, ["nobody@earth"], "nobody@earth", body, o)
+            @assert false, "send should fail"
+        catch e
+            @test contains(string(e), errmsg)
+        end
+    end
+end
