8580c2342fa89fa8858e7b0299599501a6942f13
Ricks-ha <ricks.ha@web.de>
2018-11-16 18:28:55 +0800

    Move test to a big test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,32 +2,33 @@ using Test
 using Distributed
 using SMTPClient
 
+@testset "Errors" begin
+    @testset "Error message for Humans(TM)" begin
+        let errmsg = "Couldn't resolve host name"
+            server = "smtp://nonexists"
+            body = IOBuffer("test")
 
-@testset "Error message for Humans(TM)" begin
-    let errmsg = "Couldn't resolve host name"
-        server = "smtp://nonexists"
-        body = IOBuffer("test")
-
-        try
-            send(server, ["nobody@earth"], "nobody@earth", body)
-            @assert false, "send should fail"
-        catch e
-            @test occursin(string(errmsg), string(e))
+            try
+                send(server, ["nobody@earth"], "nobody@earth", body)
+                @assert false, "send should fail"
+            catch e
+                @test occursin(string(errmsg), string(e))
+            end
         end
     end
-end
 
 
-@testset "Non-blocking send" begin
-    let errmsg = "Couldn't resolve host name"
-        opt = SendOptions(blocking = false)
-        server = "smtp://nonexists"
-        body = IOBuffer("test")
+    @testset "Non-blocking send" begin
+        let errmsg = "Couldn't resolve host name"
+            opt = SendOptions(blocking = false)
+            server = "smtp://nonexists"
+            body = IOBuffer("test")
 
-        future = send(server, ["nobody@earth"], "nobody@earth", body, opt)
-        @test future isa Future
+            future = send(server, ["nobody@earth"], "nobody@earth", body, opt)
+            @test future isa Future
 
-        e = fetch(future)
-        @test occursin(string(errmsg), string(e))
+            e = fetch(future)
+            @test occursin(string(errmsg), string(e))
+        end
     end
 end
