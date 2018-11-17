cc019058a56a63da45c5fdc3a569ad83f446e4f4
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-11-17 20:35:16 +0800

    implement fake smtp server via Python
    close #12

--- test/send.jl
+++ test/send.jl
@@ -13,19 +13,37 @@ end
   logfile = tempname()
   server = "smtp://localhost:1025"
   addr = "<julian@julialang.org>"
+  mock = joinpath(dirname(@__FILE__), "mock.py")
 
-  cmd = `smtp-sink -v -D $logfile localhost:1025 10`
+  cmd = `python3 $mock $logfile`
   smtpsink = run(cmd, wait = false)
-  sleep(.5)  # wait for smtp-sink ready
+  sleep(.5)  # wait for fake smtp server ready
 
   try
     let  # send with body::IOBuffer
       opts = SendOptions(blocking = true)
       body = IOBuffer("body::IOBuffer test")
-      resp = send(server, [addr], addr, body, opts)
+      send(server, [addr], addr, body, opts)
       test_content(logfile) do s
         @test occursin("body::IOBuffer test", s)
       end
+    end
+
+    let
+      opts = SendOptions(blocking = true,
+                         username = "foo@example.org", passwd = "bar")
+      body = IOBuffer("AUTH PLAIN test")
+      send(server, [addr], addr, body, opts)
+      test_content(logfile) do s
+        @test occursin("AUTH PLAIN test", s)
+      end
+    end
+
+    let
+      opts = SendOptions(blocking = true,
+                         username = "foo@example.org", passwd = "invalid")
+      body = IOBuffer("invalid password")
+      @test_throws Exception send(server, [addr], addr, body, opts)
     end
 
   finally
